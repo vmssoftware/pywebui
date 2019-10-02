314a8060c635bd49d15d145269e871a893c3a96c
Viktor Ikonnikov <viktor.ikonnikov@gmail.com>
2019-10-02 12:00:12 +0500

    Setup script added

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='pywebui',
+      version='0.1',
+      description='Python bind for OpenVMS Administration Tool',
+      author='VMS Software',
+      author_email='info@vmssoftware.com',
+      packages=['pywebui'],
+      install_requires=['requests==2.22.0'],
+      setup_requires=['requests==2.22.0']
+)
