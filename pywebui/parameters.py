05810d478cb2c80efdd592aa39764c82537a4a3f
Viktor Ikonnikov <viktor.ikonnikov@gmail.com>
2020-10-24 22:15:34 +0500

    Docstrings for system parameters added

--- pywebui/parameters.py
+++ pywebui/parameters.py
@@ -1,15 +1,31 @@
+from typing import List
+
 from pywebui import urls
 from pywebui.exceptions import ConnectorException
 from pywebui.response import ResponseObject
 
 
 class SystemParameter(ResponseObject):
+    """SystemParameter object.
+
+    Attributes:
+        currentValue (str): The current value of system parameter.
+        defaultValue (str): The default value of system parameter.
+        dynamic	(bool): The parameter attribute which means that value can be modified (true or false).
+        maxValue (str): The maximum value of system parameter.
+        minValue (str): The minimum value of system parameter.
+        name (str): The name of system parameter.
+        unit (str): The unit of system parameter.
+    """
     def __repr__(self):
         return f'{self.name}={self.currentValue}'
 
 
 class SystemParametersMethods:
-    def get_current_parameters(self, group):
+    """Encapsulates methods for manage system parameters."""
+
+    def get_current_parameters(self, group: str) -> List[SystemParameter]:
+        """Returns the list of system parameters from the current system parameter file on disk."""
         parameters = []
         r = self.get(urls.API_GET_CURRENT_SYSGEN, group=group)
         if r.status_code == 200:
@@ -18,7 +34,8 @@ class SystemParametersMethods:
 
         return parameters
 
-    def get_active_parameters(self, group):
+    def get_active_parameters(self, group: str) -> List[SystemParameter]:
+        """Returns the list of system parameters from the active system in memory."""
         parameters = []
         r = self.get(urls.API_GET_ACTIVE_SYSGEN, group=group)
         if r.status_code == 200:
@@ -27,7 +44,8 @@ class SystemParametersMethods:
 
         return parameters
 
-    def edit_parameter(self, source, parameter, value):
+    def edit_parameter(self, source: str, parameter: str, value: str) -> bool:
+        """Edits selected system parameters."""
         data = [
             {
                 "dataSource": source,
