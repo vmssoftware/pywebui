08815e162581d9ddfe1995082d30a600f9311b32
Viktor Ikonnikov <viktor.ikonnikov@gmail.com>
2021-07-28 13:38:18 +0500

    Syntax error fixed

--- pywebui/connector.py
+++ pywebui/connector.py
@@ -68,7 +68,7 @@ class Connector(
 
     def get(self, url, **query_params):
         url = urljoin(self.host, url.format(**query_params))
-        response =git  self.session.get(url, cookies={'jwt': self.token})
+        response = self.session.get(url, cookies={'jwt': self.token})
         return response
 
     def post(self, url, json={}, **query_params):
